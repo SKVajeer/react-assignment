6f18ebef9d2ab154a7a8462d95c9788919668849
vajeer <skvajeer787@gmail.comm>
2023-11-29 16:32:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var a = document.createElement("h1")
+a.innerText="react assignment"
+var b = document.getElementById("root")
+b.appendChild(a)
+let  x = React.createElement("h1",{},"hello world this text add by react" )
+let y =  document.getElementById("root1")
+let z = ReactDOM.createRoot(y)
+z.render(x)
