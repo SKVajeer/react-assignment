8f59f62ade733f88541ef28815533e8580dd4512
vajeer <skvajeer787@gmail.comm>
2023-11-29 17:32:28 +0530

    updated

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,10 @@
+var a = document.createElement("h1");
+a.innerText = "react assignment";
+var b = document.getElementById("root");
+b.appendChild(a);
+let x = React.createElement("h1", {}, "hello world this text add by react");
+let y = document.getElementById("root1");
+let z = ReactDOM.createRoot(y);
+z.render(x);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
