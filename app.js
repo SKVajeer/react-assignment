da3afece7c4e12b83f5aee1cbb6337c4cce38d6f
vajeer <skvajeer787@gmail.comm>
2023-11-29 20:32:26 +0530

    updated

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ let  x = React.createElement("h1",{},"hello world this text add by react" )
 let y =  document.getElementById("root1")
 let z = ReactDOM.createRoot(y)
 z.render(x)
+// create nested React Element
+var e = React.createElement("h1",{},"important notice");
+var f = React.createElement("p",{},"we can add paragraph");
+var root = document.getElementById("root2");
+var react = ReactDOM.createRoot(root);
+react.render(React.createElement('div',{},e,f));
